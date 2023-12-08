a5e8421e64888ae45057266bf6a9dbf6828ed848
Henrique Artur <contato@henriqueartur.com>
2023-12-07 21:31:08 -0300

    :bug: Fix test imports

--- lib/tests/index.test.ts
+++ lib/tests/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from "vitest";
-import { helloWorld } from "../src";
+import { helloWorld } from "@src/index.js";
 
 it("should return Hello, World!", () => {
   expect(helloWorld()).toBe("Hello, World!");
