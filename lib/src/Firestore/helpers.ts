2b11dfa136dfe3f2b746378f05e6a9bce94ad0eb
Henrique Artur <contato@henriqueartur.com>
2023-12-10 10:21:39 -0300

    :sparkles: FormatDate/2

--- lib/src/Firestore/helpers.ts
+++ lib/src/Firestore/helpers.ts
@@ -1,23 +1,49 @@
 import { ID } from "@src/types.js";
-import { DBDocument, DocFormat, FirestoreDoc, OptState, TimestampsData } from "./FirestoreTypes.js";
+import { DBDocument, DocFormatFirestore, OptState } from "./FirestoreTypes.js";
+import { Timestamp } from "firebase/firestore";
 
 export function formatData<T extends object, O extends OptState<"TIMESTAMP">>(
   anId: ID,
-  aData: DocFormat<T, O>,
-  addTimestamps?: boolean
-) {
-  if (addTimestamps) {
-    const aDataWithTimestamp = aData as FirestoreDoc<T> & TimestampsData;
-    return {
-      _id: anId,
-      ...aDataWithTimestamp,
-      _createdAt: aDataWithTimestamp._createdAt.toDate(),
-      _updatedAt: aDataWithTimestamp._updatedAt.toDate()
-    } as DBDocument<T, O>;
-  }
-  const aDataWithoutTimestamp = aData as FirestoreDoc<T>;
+  aData: DocFormatFirestore<T, O>
+): DBDocument<T, O> {
   return {
     _id: anId,
-    ...aDataWithoutTimestamp
-  } as DBDocument<T, O>;
+    ...parseTimestampToDate(aData)
+  };
+}
+
+function parseTimestampToDate(aData: any): any {
+  if (Array.isArray(aData)) {
+    return aData.map((v) => parseTimestampToDate(v));
+  }
+  if (aData instanceof Timestamp) {
+    return aData.toDate();
+  }
+  if (
+    typeof aData == "bigint" ||
+    typeof aData == "boolean" ||
+    typeof aData == "function" ||
+    typeof aData == "number" ||
+    typeof aData == "string" ||
+    typeof aData == "symbol" ||
+    typeof aData == "undefined" ||
+    aData == null
+  ) {
+    return aData;
+  }
+  for (const [key, value] of Object.entries(aData)) {
+    if (value instanceof Timestamp) {
+      aData[key] = value.toDate();
+      continue;
+    }
+    if (Array.isArray(value)) {
+      aData[key] = value.map((v) => parseTimestampToDate(v));
+      continue;
+    }
+    if (typeof value == "object") {
+      aData[key] = parseTimestampToDate(value);
+      continue;
+    }
+  }
+  return aData;
 }
