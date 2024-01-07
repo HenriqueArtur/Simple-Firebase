103933c0c85c16784288ee11fe51ab5f70cfc4a8
Henrique Artur <contato@henriqueartur.com>
2024-01-07 16:32:28 -0300

    :recycle: Remove const from collections funtions

--- lib/src/Firestore/Collection.ts
+++ lib/src/Firestore/Collection.ts
@@ -1,5 +1,6 @@
 import { CollectionReference, Firestore, collection } from "firebase/firestore";
 import { BuildFunctions } from "./CollectionFunctions.js";
+import { Deep } from "@src/types.js";
 
 export interface CollectionOptions {
   customId: boolean;
@@ -21,3 +22,17 @@ export function SubCollection<T extends object, SC extends Record<string, object
 ) {
   return BuildFunctions<T, SC>(collection(aParentCollection, aPath), anOptions);
 }
+
+/* HANDLERS */
+export function setOptions(anOptions?: Deep<CollectionOptions>): CollectionOptions {
+  if (!anOptions) {
+    return {
+      customId: false,
+      addTimestamps: true,
+    }
+  }
+  return {
+    customId: "customId" in anOptions ? (anOptions.customId as boolean) : false,
+    addTimestamps: "addTimestamps" in anOptions ? (anOptions.addTimestamps as boolean) : true
+  };
+};
