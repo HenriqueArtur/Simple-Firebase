190779b30d2c38984e238fe6607d85bff7537d18
Henrique Artur <contato@henriqueartur.com>
2024-01-07 21:14:13 -0300

    :technologist: FirebaseObject/0 test helper

--- lib/tests/__HELPERS__/firestoreTestsHelpers.ts
+++ lib/tests/__HELPERS__/firestoreTestsHelpers.ts
@@ -1,3 +1,7 @@
+import { BuildFirebase } from "@src/Services.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Firestore, collection, deleteDoc, getDocs, query } from "firebase/firestore";
 
 export async function cleanCollections(
@@ -11,4 +15,19 @@ export async function cleanCollections(
       await deleteDoc(doc.ref);
     }
   }
+}
+
+export async function FirebaseObject() {
+  return await BuildFirebase(
+    {
+      apiKey: process.env!.API_KEY as string,
+      authDomain: process.env!.AUTH_DOMAIN as string,
+      projectId: process.env!.PROJECT_ID as string,
+      storageBucket: process.env!.STORAGE_BUCKET as string,
+      messagingSenderId: process.env!.MESSAGING_SENDER_ID as string,
+      appId: process.env!.APP_ID as string
+    },
+    "Simple Firebase",
+    "test"
+  );
 }
