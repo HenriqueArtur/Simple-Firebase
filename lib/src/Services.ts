c68fad7d5593a5ec790f0f30fcf156bf68572b07
Henrique Artur <contato@henriqueartur.com>
2024-01-08 10:26:11 -0300

    :sparkles: Add firebase ports to build

--- lib/src/Services.ts
+++ lib/src/Services.ts
@@ -18,15 +18,44 @@ export interface FirebaseEntity {
   FIRESTORE_WEB: Firestore;
 }
 
+export interface EnvOptions {
+  env: "test" | "prod";
+  port?: {
+    auth?: number;
+    functions?: number;
+    firestore?: number;
+    hosting?: number;
+  };
+}
+
+interface EnvOptionsFilled {
+  env: "test" | "prod";
+  port: {
+    auth: number;
+    functions: number;
+    firestore: number;
+    hosting: number;
+  };
+}
+
 export async function BuildFirebase(
   firebaseConfig: FirebaseConfig,
   anAppName: string,
-  ENV: "test" | "prod"
+  opts: EnvOptions
 ): Promise<FirebaseEntity> {
+  const optFilled: EnvOptionsFilled = {
+    env: opts.env,
+    port: {
+      auth: opts.port && opts.port.auth ? opts.port.auth : 9099,
+      functions: opts.port && opts.port.functions ? opts.port.functions : 5001,
+      firestore: opts.port && opts.port.firestore ? opts.port.firestore : 8080,
+      hosting: opts.port && opts.port.hosting ? opts.port.hosting : 5000
+    }
+  };
   const anApp = appExists(anAppName);
   return anApp
     ? getServicesRef(anApp)
-    : await buildServices(initializeApp(firebaseConfig, anAppName), ENV);
+    : await buildServices(initializeApp(firebaseConfig, anAppName), optFilled);
 }
 
 function appExists(name: string): FirebaseApp | undefined {
@@ -37,18 +66,18 @@ function appExists(name: string): FirebaseApp | undefined {
   }
 }
 
-async function buildServices(firebaseRef: FirebaseApp, ENV: "test" | "prod") {
+async function buildServices(firebaseRef: FirebaseApp, opts: EnvOptionsFilled) {
   const AUTH_WEB = getAuth(firebaseRef);
   const CLOUD_FUNCTIONS_WEB = getFunctions(firebaseRef);
   const FIRESTORE_WEB = getFirestore(firebaseRef);
-  if (ENV == "test") {
-    const authUrl = "http://localhost:9099";
+  if (opts.env == "test") {
+    const authUrl = `http://localhost:${opts.port.auth}`;
     await fetch(authUrl);
     connectAuthEmulator(AUTH_WEB, authUrl, {
       disableWarnings: true
     });
-    connectFunctionsEmulator(CLOUD_FUNCTIONS_WEB, "localhost", 5001);
-    connectFirestoreEmulator(FIRESTORE_WEB, "localhost", 8080);
+    connectFunctionsEmulator(CLOUD_FUNCTIONS_WEB, "localhost", opts.port.functions);
+    connectFirestoreEmulator(FIRESTORE_WEB, "localhost", opts.port.firestore);
   }
   return { AUTH_WEB, CLOUD_FUNCTIONS_WEB, FIRESTORE_WEB };
 }
