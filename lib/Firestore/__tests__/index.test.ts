739f114aedd9dc0cd7c5bd0b4de485061f3d869d
Henrique Artur <contato@henriqueartur.com>
2024-05-01 11:11:02 -0300

    ✅ ajust tests BuildFirebase to new logic

--- lib/Firestore/__tests__/index.test.ts
+++ lib/Firestore/__tests__/index.test.ts
@@ -1,13 +1,14 @@
-import { BuildFirestore } from "@src/Firestore/index.js";
-import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js";
-import { describe, expect, it } from "vitest";
+import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
+import { BuildFirestore } from "@src/Firestore/index.js"
+import { describe, expect, it } from "vitest"
 
 describe("FIREBASE Firestore", async () => {
-  const { FIRESTORE_WEB } = await FirebaseObject();
+  const { FIRESTORE_WEB } = await FirebaseObject()
 
   it("should instantiate Firebase Firestore", async () => {
-    const repository = BuildFirestore(FIRESTORE_WEB);
-    expect(Object.keys(repository)).toHaveLength(1);
-    expect(repository).toHaveProperty("collection");
-  });
-});
+    const repository = BuildFirestore(FIRESTORE_WEB)
+    const repository_keys_size = 1
+    expect(Object.keys(repository)).toHaveLength(repository_keys_size)
+    expect(repository).toHaveProperty("collection")
+  })
+})
