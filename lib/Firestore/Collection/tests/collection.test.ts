8b39e85c1719163408f80b5108d65d538c8113f7
Henrique Artur <contato@henriqueartur.com>
2024-05-01 15:10:56 -0300

    ✅ test to Collection/3

---
+++ lib/Firestore/Collection/tests/collection.test.ts
@@ -0,0 +1,27 @@
+import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
+import { CreateASchema, SCHEMA } from "@src/Firestore/Schema/index.js"
+import { describe, expect, it } from "vitest"
+
+import { Collection } from "../collection.js"
+
+describe('Collection', async () => {
+  const { FIRESTORE_WEB } = await FirebaseObject()
+
+  describe('Collection', () => {
+    it('should return a SimpleCollection object with correct properties', () => {
+      const a_firestore = FIRESTORE_WEB
+      const a_path = 'test'
+      const a_schema = CreateASchema({
+        test: SCHEMA.string()
+      })
+      const result = Collection(a_firestore, a_path, a_schema)
+      expect(result).toBeDefined()
+      expect(typeof result).toBe('object')
+      expect(result.$COLLECTION).toBeDefined()
+      expect(result.$PATH).toBeDefined()
+      expect(result.$PATH).toBe(a_path)
+      expect(result.$SCHEMA).toBeDefined()
+      expect(result.$SCHEMA).toBe(a_schema)
+    })
+  })
+})
