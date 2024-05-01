2e3dba2da7d769a654b561ed21b2a86cc11779b8
Henrique Artur <contato@henriqueartur.com>
2024-05-01 15:08:51 -0300

    ♻️ Type a SchemaShape

--- lib/Firestore/Schema/index.ts
+++ lib/Firestore/Schema/index.ts
@@ -1,21 +1,22 @@
 import { z } from "zod"
 
-export type SchemaType = z.ZodRawShape
+export type SchemaShape = z.ZodRawShape
+export type SchemaType = z.ZodSchema
 
 export const SCHEMA = {
   array: <T extends z.ZodTypeAny>(a_type: T) => z.array(a_type),
   boolean: () => z.boolean(),
   date: () => z.date(),
-  map: <T extends z.ZodRawShape>(a_map_schema: T) => z.object(a_map_schema),
+  map: <T extends SchemaShape>(a_map_schema: T) => z.object(a_map_schema),
   null: () => z.null(),
   number: () => z.number(),
   string: () => z.string(),
 }
 
-export function CreateASchema<T extends z.ZodRawShape>(a_schema: T) {
+export function CreateASchema<T extends SchemaShape>(a_schema: T): SchemaType {
   return z.object(a_schema)
 }
 
-export function validateSchema<T extends z.ZodTypeAny>(a_schema: T, a_data: unknown) {
+export function validateSchema<T extends SchemaType>(a_schema: T, a_data: unknown) {
   return a_schema.parse(a_data) as z.infer<T>
 }
