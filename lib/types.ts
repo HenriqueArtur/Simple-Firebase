975ff3197ee8f1484db996db107b0c77545e47c2
Henrique Artur <contato@henriqueartur.com>
2024-05-01 10:14:17 -0300

    🚨 fix lint on general types

--- lib/types.ts
+++ lib/types.ts
@@ -1,14 +1,14 @@
-export type ID = string;
-export type Email = `${string}@${string}.${string}`;
+export type ID = string
+export type Email = `${string}@${string}.${string}`
 
-export type Service = "AUTH" | "FIRESTORE";
+export type Service = "AUTH" | "FIRESTORE"
 
 export type Deep<T> = {
-  [K in keyof T]?: T[K] extends (infer U)[]
-    ? Deep<U>[]
-    : T[K] extends Date
-    ? Date
-    : T[K] extends object
-    ? Deep<T[K]>
-    : T[K];
-};
+  [K in keyof T]?: T[K] extends Array<infer U>
+  ? Array<Deep<U>>
+  : T[K] extends Date
+  ? Date
+  : T[K] extends object
+  ? Deep<T[K]>
+  : T[K]
+}
