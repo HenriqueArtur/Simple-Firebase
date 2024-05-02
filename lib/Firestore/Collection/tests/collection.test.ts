73ebed0f1a3f6e0c25d8f5d5bc7db7108ea3e0e4
Henrique Artur <contato@henriqueartur.com>
2024-05-01 22:23:05 -0300

    ✅ update collections test to new _type

--- lib/Firestore/Collection/tests/collection.test.ts
+++ lib/Firestore/Collection/tests/collection.test.ts
@@ -2,19 +2,19 @@ import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
 import { CreateASchema, SCHEMA } from "@src/Firestore/Schema/index.js"
 import { describe, expect, it } from "vitest"
 
-import { Collection } from "../collection.js"
+import { FactoryCollection } from "../collection.js"
 
 describe('Collection', async () => {
   const { FIRESTORE_WEB } = await FirebaseObject()
 
-  describe('Collection', () => {
+  describe('FactoryCollection', () => {
     it('should return a SimpleCollection object with correct properties', () => {
       const a_firestore = FIRESTORE_WEB
       const a_path = 'test'
       const a_schema = CreateASchema({
         test: SCHEMA.string()
       })
-      const result = Collection(a_firestore, a_path, a_schema)
+      const result = FactoryCollection(a_firestore, a_path, a_schema)
       expect(result).toBeDefined()
       expect(typeof result).toBe('object')
       expect(result.$COLLECTION).toBeDefined()
