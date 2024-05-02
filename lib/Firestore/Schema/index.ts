960c826bb52d15a311f5f5a39733011b791fdd03
Henrique Artur <contato@henriqueartur.com>
2024-05-01 22:24:07 -0300

    🐛 fix SimpleSchema return

--- lib/Firestore/Schema/index.ts
+++ lib/Firestore/Schema/index.ts
@@ -3,6 +3,8 @@ import { z } from "zod"
 export type SchemaShape = z.ZodRawShape
 export type SchemaType = z.ZodSchema
 
+export type SimpleSchema<T extends SchemaShape> = z.ZodObject<T>
+
 export const SCHEMA = {
   array: <T extends z.ZodTypeAny>(a_type: T) => z.array(a_type),
   boolean: () => z.boolean(),
@@ -13,7 +15,7 @@ export const SCHEMA = {
   string: () => z.string(),
 }
 
-export function CreateASchema<T extends SchemaShape>(a_schema: T): SchemaType {
+export function CreateASchema<T extends SchemaShape>(a_schema: T): SimpleSchema<T> {
   return z.object(a_schema)
 }
 
