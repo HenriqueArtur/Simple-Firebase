cd37185a8260f93c2bfdbe0f67e1e49c035ba7ab
Henrique Artur <contato@henriqueartur.com>
2024-05-01 22:22:23 -0300

    ✨ add type to collection

--- lib/Firestore/Collection/collection.ts
+++ lib/Firestore/Collection/collection.ts
@@ -1,22 +1,41 @@
 import { type Path } from "@src/types.js"
 import { collection, type CollectionReference, type Firestore } from "firebase/firestore"
+import { type z } from "zod"
 
-import { type SchemaType } from "../Schema/index.js"
+import { type SchemaShape, type SimpleSchema } from "../Schema/index.js"
 
-export interface SimpleCollection {
+export interface SimpleCollection<T extends SchemaShape> {
+  readonly _type: z.infer<SimpleSchema<T>>
   readonly $COLLECTION: CollectionReference
   readonly $PATH: Path
-  readonly $SCHEMA: SchemaType
+  readonly $SCHEMA: SimpleSchema<T>
 }
 
-export function Collection(
+export function FactoryCollection<T extends SchemaShape>(
   a_firestore_ref: Firestore,
   a_path: Path,
-  a_schema: SchemaType
-): SimpleCollection {
-  return {
-    $COLLECTION: collection(a_firestore_ref, a_path),
-    $PATH: a_path,
-    $SCHEMA: a_schema
+  a_schema: SimpleSchema<T>
+): SimpleCollection<T> {
+  return new ConcreteSimpleCollection(
+    a_firestore_ref,
+    a_path,
+    a_schema
+  )
+}
+
+class ConcreteSimpleCollection<T extends SchemaShape> {
+  readonly _type!: z.infer<SimpleSchema<T>>
+  readonly $COLLECTION: CollectionReference
+  readonly $PATH: Path
+  readonly $SCHEMA: SimpleSchema<T>
+
+  constructor(
+    a_firestore_ref: Firestore,
+    a_path: Path,
+    a_schema: SimpleSchema<T>
+  ) {
+    this.$COLLECTION = collection(a_firestore_ref, a_path)
+    this.$PATH = a_path
+    this.$SCHEMA = a_schema
   }
 }
