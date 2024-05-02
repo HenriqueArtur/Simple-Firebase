022b4612da49f0856b2fb049a4c70fb71e531f1b
Henrique Artur <contato@henriqueartur.com>
2024-05-01 22:25:29 -0300

    ✨ ConcreteSimpleCollection

--- lib/Firestore/Document/index.ts
+++ lib/Firestore/Document/index.ts
@@ -1,17 +1,18 @@
 import { type ID } from "@src/types.js"
 
 import { type SimpleCollection } from "../Collection/collection.js"
+import { type SchemaShape } from "../Schema/index.js"
 
-export interface SimpleDocument extends SimpleCollection {
+export interface SimpleDocument<T extends SchemaShape> extends SimpleCollection<T> {
   readonly id: ID;
-  readonly data: object;
+  readonly data: SimpleCollection<T>["_type"];
 }
 
-export function FactorySimpleDocument(
-  a_collection: SimpleCollection,
+export function FactorySimpleDocument<T extends SchemaShape>(
+  a_collection: SimpleCollection<T>,
   a_id: ID,
-  a_document_data: object
-): SimpleDocument {
+  a_document_data: SimpleCollection<T>["_type"]
+): SimpleDocument<T> {
   return {
     ...a_collection,
     data: a_document_data,
