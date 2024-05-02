bb6a1baf6a11bfffb4a2f645b9a4301ee56120da
Henrique Artur <contato@henriqueartur.com>
2024-05-01 22:23:27 -0300

    🤡 CollectionMock

---
+++ lib/Firestore/Collection/collection-mock.ts
@@ -0,0 +1,16 @@
+import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
+
+import { CreateASchema, SCHEMA, type SchemaShape, type SimpleSchema } from "../Schema/index.js"
+import { FactoryCollection } from "./collection.js"
+
+export async function CollectionMock<T extends SchemaShape>(
+  a_schema?: SimpleSchema<T>
+) {
+  const { FIRESTORE_WEB } = await FirebaseObject()
+  const the_firestore = FIRESTORE_WEB
+  const the_path = 'test'
+  const the_schema = a_schema ?? CreateASchema({
+    test: SCHEMA.string()
+  })
+  return FactoryCollection(the_firestore, the_path, the_schema as SimpleSchema<T>)
+}
