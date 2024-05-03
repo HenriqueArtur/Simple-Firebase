6117239ebae594ce4f729a1229a8f754c8de163c
Henrique Artur <contato@henriqueartur.com>
2024-05-02 22:33:34 -0300

    🚧 WIP create collection

--- lib/Firestore/Collection/create.ts
+++ lib/Firestore/Collection/create.ts
@@ -1,13 +1,39 @@
 import { addDoc, type CollectionReference, getDoc } from "firebase/firestore/lite"
 
-export async function create(
+import { FactorySimpleDocument } from "../Document/index.js"
+import { type SchemaShape } from "../Schema/index.js"
+import { type SimpleCollection, type SimpleCollectionBase } from "./collection.js"
+
+export async function Create<T extends SchemaShape>(
+  a_simple_collection: SimpleCollectionBase<T>,
+  a_new_data: SimpleCollection<T>["_type"]
+) {
+  const {
+    data,
+    id,
+    reference
+  } = await createInFirebaseAutoId<SimpleCollection<T>["_type"]>(
+    a_simple_collection.$COLLECTION,
+    a_new_data
+  )
+  return FactorySimpleDocument(
+    a_simple_collection,
+    id,
+    data,
+    reference
+  )
+}
+
+export async function createInFirebaseAutoId<T>(
   a_collection: CollectionReference,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  a_new_data: any
+  a_new_data: object
 ) {
   const a_doc_ref = await addDoc(a_collection, a_new_data)
   const a_doc_snap = await getDoc(a_doc_ref)
-  return a_doc_snap
+  return {
+    data: a_doc_snap.data() as T | undefined,
+    id: a_doc_snap.id,
+    reference: a_doc_snap.ref
+  }
 }
-
 
