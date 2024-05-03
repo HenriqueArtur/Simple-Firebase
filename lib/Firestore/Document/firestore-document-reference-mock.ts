f8ef38ecd94be05d13f9c86914bd47ca3f477294
Henrique Artur <contato@henriqueartur.com>
2024-05-02 22:16:13 -0300

    🤡 FirestoreDocumentReferenceMock

---
+++ lib/Firestore/Document/firestore-document-reference-mock.ts
@@ -0,0 +1,29 @@
+import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
+import { type CollectionReference, type DocumentReference } from "firebase/firestore"
+
+export async function FactoryFirestoreDocumentReferenceMock(): Promise<DocumentReference> {
+  const { FIRESTORE_WEB } = await FirebaseObject()
+  const a_collection: CollectionReference = {
+    converter: null,
+    firestore: FIRESTORE_WEB,
+    id: "id",
+    parent: null,
+    path: "test",
+    type: "collection",
+    withConverter: () => {
+      throw new Error("Function not implemented.")
+    },
+  }
+  const a_document: DocumentReference = {
+    converter: null,
+    firestore: FIRESTORE_WEB,
+    id: "id",
+    parent: a_collection,
+    path: "test",
+    type: "document",
+    withConverter: () => {
+      throw new Error("Function not implemented.")
+    }
+  }
+  return a_document
+}
