786d6243716538034f991bc32a12f59d2004f0f4
Henrique Artur <contato@henriqueartur.com>
2024-05-02 22:16:43 -0300

    ✨ add  to SimpleDocument

--- lib/Firestore/Document/index.ts
+++ lib/Firestore/Document/index.ts
@@ -1,20 +1,24 @@
 import { type ID } from "@src/types.js"
+import { type DocumentReference } from "firebase/firestore"
 
-import { type SimpleCollection } from "../Collection/collection.js"
+import { type SimpleCollection, type SimpleCollectionBase } from "../Collection/collection.js"
 import { type SchemaShape } from "../Schema/index.js"
 
-export interface SimpleDocument<T extends SchemaShape> extends SimpleCollection<T> {
-  readonly id: ID;
-  readonly data: SimpleCollection<T>["_type"];
+export interface SimpleDocument<T extends SchemaShape> extends SimpleCollectionBase<T> {
+  readonly id: ID
+  readonly data: SimpleCollection<T>["_type"]
+  readonly $REFERENCE: DocumentReference
 }
 
 export function FactorySimpleDocument<T extends SchemaShape>(
   a_collection: SimpleCollection<T>,
   a_id: ID,
-  a_document_data: SimpleCollection<T>["_type"]
+  a_document_data: SimpleCollection<T>["_type"],
+  a_reference: DocumentReference
 ): SimpleDocument<T> {
   return {
     ...a_collection,
+    $REFERENCE: a_reference,
     data: a_document_data,
     id: a_id,
   }
