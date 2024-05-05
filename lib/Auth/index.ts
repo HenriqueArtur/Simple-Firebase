baaa6560c0467e3f58c99aee7591a68ce55786f0
Henrique Artur <contato@henriqueartur.com>
2024-05-05 15:42:38 -0300

    🚨 fix auth lint

--- lib/Auth/index.ts
+++ lib/Auth/index.ts
@@ -1,17 +1,15 @@
-import { Email } from "@src/types.js";
-import { Auth, signInWithEmailAndPassword } from "firebase/auth";
+import { type Email } from "@src/types.js"
+import { type Auth, signInWithEmailAndPassword } from "firebase/auth"
 
-export function BuildFirebaseAuth(aFirebaseAuth: Auth) {
+export function BuildFirebaseAuth(a_firebase_auth: Auth) {
   return {
-    signInWithEmail: async (email: Email, password: string) => {
-      return await signInWithEmailAndPassword(aFirebaseAuth, email, password);
-    },
-    signOut: async () => await aFirebaseAuth.signOut(),
+    signInWithEmail: async (email: Email, password: string) =>
+      await signInWithEmailAndPassword(a_firebase_auth, email, password),
+    signOut: async () => { await a_firebase_auth.signOut() },
     isLoggedIn: () => {
-      if (!aFirebaseAuth.currentUser) {
-        return null;
-      }
-      return aFirebaseAuth.currentUser;
+      if (a_firebase_auth.currentUser === null)
+        return null
+      return a_firebase_auth.currentUser
     }
-  };
+  }
 }
