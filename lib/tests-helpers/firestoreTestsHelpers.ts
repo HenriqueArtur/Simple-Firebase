6350ef14a619027883bbdd47501c96624c3b28f3
Henrique Artur <contato@henriqueartur.com>
2024-05-05 15:12:02 -0300

    🚚 pass old firestore tests files to tests-helpers

---
+++ lib/tests-helpers/firestoreTestsHelpers.ts
@@ -0,0 +1,18 @@
+import { BuildFirebase } from "@src/Services.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+export async function FirebaseObject() {
+  return await BuildFirebase(
+    {
+      apiKey: process.env.API_KEY!,
+      authDomain: process.env.AUTH_DOMAIN!,
+      projectId: process.env.PROJECT_ID!,
+      storageBucket: process.env.STORAGE_BUCKET!,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID!,
+      appId: process.env.APP_ID!
+    },
+    "Simple Firebase",
+    { env: "test" }
+  )
+}
