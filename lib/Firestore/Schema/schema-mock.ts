6ba888ead66508be1443daa7af7b7839d479b0c1
Henrique Artur <contato@henriqueartur.com>
2024-05-05 12:16:41 -0300

    🤡 SchemaMock

--- lib/Firestore/Schema/schema-mock.ts
+++ lib/Firestore/Schema/schema-mock.ts
@@ -1,12 +1,16 @@
 import { CreateASchema, SCHEMA } from "./schema.js"
 
+const DEFAULT_SCHEMA_SHAPE_MOCK = {
+  array: SCHEMA.string().array(),
+  boolean: SCHEMA.boolean(),
+  date: SCHEMA.date(),
+  null: SCHEMA.null(),
+  number: SCHEMA.number(),
+  text: SCHEMA.string(),
+}
+
+export type DefaultSchemaMock = typeof DEFAULT_SCHEMA_SHAPE_MOCK
+
 export function DEFAULT_SCHEMA_MOCK() {
-  return CreateASchema({
-    array: SCHEMA.string().array(),
-    boolean: SCHEMA.boolean(),
-    date: SCHEMA.date(),
-    null: SCHEMA.null(),
-    number: SCHEMA.number(),
-    text: SCHEMA.string(),
-  })
+  return CreateASchema(DEFAULT_SCHEMA_SHAPE_MOCK)
 }
