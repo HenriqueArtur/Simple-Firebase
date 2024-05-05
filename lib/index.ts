1a1daa7c1446fa2ea5d98ddb4f7d943dd24f6f67
Henrique Artur <contato@henriqueartur.com>
2024-05-05 15:37:24 -0300

    ⚰️ remove old firestore import

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,2 @@
-export * as SimpleAuth from "@src/Auth/index.js";
-export * as SimpleFirestore from "@src/Firestore/index.js";
-export * as Services from "@src/Services.js";
+export * as SimpleAuth from "@src/Auth/index.js"
+export * as Services from "@src/Services.js"
