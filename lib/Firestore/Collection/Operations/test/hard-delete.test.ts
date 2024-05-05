4635f5545dc595d950aad01ab6eb0e2b8f81d535
Henrique Artur <contato@henriqueartur.com>
2024-05-05 20:54:21 -0300

    ✅ hard delete tests

---
+++ lib/Firestore/Collection/Operations/test/hard-delete.test.ts
@@ -0,0 +1,26 @@
+import { BuildServicesTest } from "@src/tests-helpers/build-services.js"
+import { doc, getDoc } from "firebase/firestore"
+import { afterAll, describe, expect, it } from "vitest"
+
+import { CollectionBaseDefaultMock } from "../../collection-mock.js"
+import { hardDelete } from "../hard-delete.js"
+import { cleanCollections } from "../tests-helpers/clean-collections.js"
+import { createATestDefaultObject } from "../tests-helpers/create-test-default-object.js"
+
+describe("Firestore Hard Delete", async () => {
+  afterAll(async () => {
+    const { FIRESTORE_WEB } = await BuildServicesTest()
+    await cleanCollections(FIRESTORE_WEB, ["test"])
+  })
+
+  const $SIMPLE_COLLECTION = await CollectionBaseDefaultMock()
+
+  it("Should delete a Document", async () => {
+    const an_id = "a_id"
+    const a_data = { key: "value" }
+    await createATestDefaultObject($SIMPLE_COLLECTION.$COLLECTION, a_data, an_id)
+    await hardDelete($SIMPLE_COLLECTION, an_id)
+    const a_doc = await getDoc(doc($SIMPLE_COLLECTION.$COLLECTION, an_id))
+    expect(a_doc.exists()).toBeFalsy()
+  })
+})
