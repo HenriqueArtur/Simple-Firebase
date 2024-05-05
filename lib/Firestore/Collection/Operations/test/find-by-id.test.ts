111831e71886814533a1f31a21cd94a25d7f3a61
Henrique Artur <contato@henriqueartur.com>
2024-05-05 20:26:46 -0300

    ✅ find by id test

---
+++ lib/Firestore/Collection/Operations/test/find-by-id.test.ts
@@ -0,0 +1,47 @@
+import { BuildServicesTest } from "@src/tests-helpers/build-services.js"
+import { afterAll, describe, expect, it } from "vitest"
+
+import { CollectionBaseDefaultMock } from "../../collection-mock.js"
+import { findById } from "../find-by-id.js"
+import { cleanCollections } from "../tests-helpers/clean-collections.js"
+import { createATestDefaultObject } from "../tests-helpers/create-test-default-object.js"
+
+describe("Firestore Find by ID", async () => {
+  afterAll(async () => {
+    const { FIRESTORE_WEB } = await BuildServicesTest()
+    await cleanCollections(FIRESTORE_WEB, ["test"])
+  })
+
+  const $SIMPLE_COLLECTION = await CollectionBaseDefaultMock()
+  const FIND_BY_ID_KEYS_LENGTH = 6
+
+  it("Shoud NOT find a Document", async () => {
+    const a_wrong_id = "a_wrong_id"
+    const response = await findById($SIMPLE_COLLECTION, a_wrong_id)
+    expect(Object.keys(response)).toHaveLength(FIND_BY_ID_KEYS_LENGTH)
+    expect(response).toHaveProperty("data")
+    expect(response.data).toBeUndefined()
+    expect(response).toHaveProperty("id")
+    expect(response.id).not.toBeNull()
+    expect(response).toHaveProperty("$COLLECTION")
+    expect(response).toHaveProperty("$PATH")
+    expect(response).toHaveProperty("$SCHEMA")
+    expect(response).toHaveProperty("$REFERENCE")
+  })
+
+  it("Shoud find a Document", async () => {
+    const an_id = "a_id"
+    const a_data = { key: "value" }
+    await createATestDefaultObject($SIMPLE_COLLECTION.$COLLECTION, a_data, an_id)
+    const response = await findById($SIMPLE_COLLECTION, an_id)
+    expect(Object.keys(response)).toHaveLength(FIND_BY_ID_KEYS_LENGTH)
+    expect(response).toHaveProperty("data")
+    expect(response.data).toStrictEqual(a_data)
+    expect(response).toHaveProperty("id")
+    expect(response.id).not.toBeNull()
+    expect(response).toHaveProperty("$COLLECTION")
+    expect(response).toHaveProperty("$PATH")
+    expect(response).toHaveProperty("$SCHEMA")
+    expect(response).toHaveProperty("$REFERENCE")
+  })
+})
