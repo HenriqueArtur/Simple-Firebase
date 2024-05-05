56fb68a2ff3ea1653254b5146ef0099d6948fa95
Henrique Artur <contato@henriqueartur.com>
2024-05-05 15:43:28 -0300

    🚨 fix auth test lint

--- lib/Auth/__tests__/index.test.ts
+++ lib/Auth/__tests__/index.test.ts
@@ -1,14 +1,14 @@
-import { BuildFirebaseAuth } from "@src/Auth/index.js";
-import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js";
-import { describe, expect, it } from "vitest";
+import { BuildFirebaseAuth } from "@src/Auth/index.js"
+import { FirebaseObject } from "@src/tests-helpers/build-services.js"
+import { describe, expect, it } from "vitest"
 
 describe("FIREBASE WEB Auth", async () => {
-  const { AUTH_WEB } = await FirebaseObject();
+  const { AUTH_WEB } = await FirebaseObject()
 
   it("should instantiate Firebase Auth", async () => {
-    const auth = BuildFirebaseAuth(AUTH_WEB);
-    expect(auth).toHaveProperty("signInWithEmail");
-    expect(auth).toHaveProperty("signOut");
-    expect(auth).toHaveProperty("isLoggedIn");
-  });
-});
+    const auth = BuildFirebaseAuth(AUTH_WEB)
+    expect(auth).toHaveProperty("signInWithEmail")
+    expect(auth).toHaveProperty("signOut")
+    expect(auth).toHaveProperty("isLoggedIn")
+  })
+})
