cff1b91795ce793b948c9baf541e1f5218b3e7cd
Henrique Artur <contato@henriqueartur.com>
2024-05-05 12:17:03 -0300

    🤡 Collection base mock

--- lib/Firestore/Collection/collection-mock.ts
+++ lib/Firestore/Collection/collection-mock.ts
@@ -1,7 +1,9 @@
 import { FirebaseObject } from "@src/__HELPERS__/firestoreTestsHelpers.js"
+import { collection } from "firebase/firestore"
 
 import { CreateASchema, SCHEMA, type SchemaShape, type SimpleSchema } from "../Schema/schema.js"
-import { FactoryCollection } from "./collection.js"
+import { DEFAULT_SCHEMA_MOCK, type DefaultSchemaMock } from "../Schema/schema-mock.js"
+import { FactoryCollection, type SimpleCollectionBase } from "./collection.js"
 
 export async function CollectionMock<T extends SchemaShape>(
   a_schema?: SimpleSchema<T>
@@ -13,4 +15,16 @@ export async function CollectionMock<T extends SchemaShape>(
     test: SCHEMA.string()
   })
   return FactoryCollection(the_firestore, the_path, the_schema as SimpleSchema<T>)
+}
+
+export async function CollectionBaseDefaultMock():
+  Promise<SimpleCollectionBase<DefaultSchemaMock>> {
+  const { FIRESTORE_WEB } = await FirebaseObject()
+  const the_path = 'test'
+  const the_schema = DEFAULT_SCHEMA_MOCK()
+  return {
+    $COLLECTION: collection(FIRESTORE_WEB, the_path),
+    $PATH: the_path,
+    $SCHEMA: the_schema
+  }
 }
