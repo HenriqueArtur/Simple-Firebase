593d8844c8f23d201297991680a11cae85825574
Henrique Artur <contato@henriqueartur.com>
2024-05-07 22:59:20 -0300

    ✅ tests for update firestore operations

---
+++ lib/Firestore/Collection/Operations/test/update.test.ts
@@ -0,0 +1,35 @@
+import { SimpleDocumentDefaultMock } from "@src/Firestore/Document/simple-document-mock.js"
+import { BuildServicesTest } from "@src/tests-helpers/build-services.js"
+import { afterAll, describe, expect, it } from "vitest"
+
+import { cleanCollections } from "../tests-helpers/clean-collections.js"
+import { updateWithId, updateWithReference } from "../update.js"
+
+describe("Firestore Update", async () => {
+  afterAll(async () => {
+    const { FIRESTORE_WEB } = await BuildServicesTest()
+    await cleanCollections(FIRESTORE_WEB, ["test"])
+  })
+
+  it("Should update with Document", async () => {
+    const a_doc = await SimpleDocumentDefaultMock()
+    const a_new_data = { text: "new" }
+    const a_new_doc = await updateWithReference(a_doc, a_new_data)
+    expect(a_new_doc.data?.text).toBe(a_new_data.text)
+  })
+
+  it("Should update with ID", async () => {
+    const a_doc = await SimpleDocumentDefaultMock()
+    const a_new_data = { text: "new" }
+    const a_new_doc = await updateWithId(
+      {
+        $COLLECTION: a_doc.$COLLECTION,
+        $SCHEMA: a_doc.$SCHEMA,
+        $PATH: a_doc.$PATH
+      },
+      a_doc.id,
+      a_new_data
+    )
+    expect(a_new_doc.data?.text).toBe(a_new_data.text)
+  })
+})
