059576cb6ad1ad0e98e27521139c6c0ab9f67bbc
Henrique Artur <contato@henriqueartur.com>
2024-05-11 14:10:40 -0300

    ✨ add negation functions

--- lib/language-functions.ts
+++ lib/language-functions.ts
@@ -1,31 +1,31 @@
 const IS_EMPTY = 0
 const MORE_THAN_ONE = 1
 
-export function includesSome<T>(an_array: T[], values_to_check: T[]): boolean {
+export function IncludesSome<T>(an_array: T[], values_to_check: T[]): boolean {
   for (const a_value of values_to_check)
     if (an_array.includes(a_value))
       return true
   return false
 }
 
-export function IsNotEmpty(
-  a_structure: unknown[] | string | object | null | undefined
-): boolean {
-  return !IsEmpty(a_structure)
-}
-
 export function IsEmpty(
   a_structure: unknown[] | string | object | null | undefined
 ): boolean {
-  if (a_structure === null || a_structure === undefined)
+  if (NotExist(a_structure))
     return true
   if (Array.isArray(a_structure))
     return ArrayIsEmpty(a_structure)
   if (typeof a_structure === "object")
-    return ObjectIsEmpty(a_structure)
+    return ObjectIsEmpty(a_structure!)
   if (typeof a_structure === "string")
     return StringIsEmpty(a_structure)
   return true
+}
+
+export function IsNotEmpty(
+  a_structure: unknown[] | string | object | null | undefined
+): boolean {
+  return !IsEmpty(a_structure)
 }
 
 export function ObjectIsEmpty(an_object: object): boolean {
@@ -41,7 +41,11 @@ export function ArrayIsEmpty(an_array: unknown[]): boolean {
 }
 
 export function Exist(a_structure: unknown): boolean {
-  return a_structure === null || a_structure === undefined
+  return !(a_structure === null || a_structure === undefined)
+}
+
+export function NotExist(a_structure: unknown): boolean {
+  return !Exist(a_structure)
 }
 
 export function SizeMoreThanOne(a_structure: string | unknown[]): boolean {
