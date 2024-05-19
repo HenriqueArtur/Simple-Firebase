2404cbbbd3e47dcd8734a98a01fbbca5ea850c39
Henrique Artur <contato@henriqueartur.com>
2024-05-19 13:06:58 -0300

    ✨ add findMany & update to collection

--- lib/Firestore/Collection/collection.ts
+++ lib/Firestore/Collection/collection.ts
@@ -1,12 +1,16 @@
-import { type ID, type Path } from "@src/types.js"
+import { type Deep, type ID, type Path } from "@src/types.js"
 import { collection, type CollectionReference, type Firestore } from "firebase/firestore"
 import { type z } from "zod"
 
 import { type SimpleDocument } from "../Document/document.js"
+import { type ManySimpleDocuments } from "../Document/many-documents.js"
+import { type SimpleQuery } from "../Query/query-types.js"
 import { type SchemaShape, type SimpleSchema } from "../Schema/schema.js"
 import { Create, type CreateCustomOptions } from "./Operations/create.js"
 import { findById } from "./Operations/find-by-id.js"
+import { findMany } from "./Operations/find-many.js"
 import { hardDelete } from "./Operations/hard-delete.js"
+import { update } from "./Operations/update.js"
 
 export interface SimpleCollectionBase<T extends SchemaShape> {
   readonly $COLLECTION: CollectionReference
@@ -20,8 +24,19 @@ export interface SimpleCollection<T extends SchemaShape> extends SimpleCollectio
     a_new_data: SimpleCollection<T>["_type"],
     a_custom_options?: CreateCustomOptions
   ) => Promise<SimpleDocument<T>>
-  readonly findById: (an_id: ID) => Promise<SimpleDocument<T>>
-  readonly hardDelete: (an_id: ID) => Promise<void>
+  readonly findById: (
+    an_id: ID
+  ) => Promise<SimpleDocument<T>>
+  readonly findMany: (
+    a_query: SimpleQuery<SimpleCollection<T>["_type"]>
+  ) => Promise<ManySimpleDocuments<T>>
+  readonly hardDelete: (
+    an_id: ID
+  ) => Promise<void>
+  readonly update: (
+    a_document: ID | SimpleDocument<T>,
+    a_new_data: Deep<SimpleCollection<T>["_type"]>
+  ) => Promise<SimpleDocument<T>>
 }
 
 export function FactoryCollection<T extends SchemaShape>(
@@ -64,11 +79,35 @@ class ConcreteSimpleCollection<T extends SchemaShape> {
   }
 
   async findById(an_id: ID) {
-    return await findById(this.$SIMPLE_COLLECTION(), an_id)
+    return await findById(
+      this.$SIMPLE_COLLECTION(),
+      an_id
+    )
+  }
+
+  async findMany(a_query: SimpleQuery<SimpleCollection<T>["_type"]>) {
+    return await findMany(
+      this.$SIMPLE_COLLECTION(),
+      a_query
+    )
   }
 
   async hardDelete(an_id: ID) {
-    await hardDelete(this.$SIMPLE_COLLECTION(), an_id)
+    await hardDelete(
+      this.$SIMPLE_COLLECTION(),
+      an_id
+    )
+  }
+
+  async update(
+    a_document: ID | SimpleDocument<T>,
+    a_new_data: Deep<SimpleCollection<T>["_type"]>
+  ) {
+    return await update(
+      this.$SIMPLE_COLLECTION(),
+      a_document,
+      a_new_data
+    )
   }
 
   private $SIMPLE_COLLECTION() {
