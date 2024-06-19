b1d074ccc53305ed89105e3db98518f5a131ae48
Henrique Artur <contato@henriqueartur.com>
2024-06-18 21:57:07 -0300

    🚧 validate string

--- lib/Firestore/Schema/types/String/string-type.ts
+++ lib/Firestore/Schema/types/String/string-type.ts
@@ -1,3 +1,6 @@
+import { Exist } from "@src/language-functions.js"
+
+import { createSimpleTypeErrorItem, type SimpleTypeErrorItem, type ValidationTuple } from "../../error/simple-type-error.js"
 import { SimpleTypeDefault } from "../Default/default-type.js"
 
 export interface SimpleString extends SimpleTypeDefault<string> {
@@ -13,4 +16,27 @@ export class SimpleString extends SimpleTypeDefault<string> {
   ) {
     super(default_value)
   }
+
+  validate(a_key: string, a_value: string): [SimpleTypeErrorItem, string] {
+    return [createSimpleTypeErrorItem(
+      a_key,
+      this.validationChain(a_value)
+    ),
+    a_value ?? this.default_value
+    ]
+  }
+
+  private validationChain(a_value: string): ValidationTuple[] {
+    return [
+      [
+        Exist(this.min_lenght) && a_value.length < this.min_lenght!,
+        `"${a_value}" has length less than "${this.min_lenght}"`
+      ],
+      [
+        Exist(this.max_lenght) && a_value.length > this.max_lenght!,
+        `"${a_value}" has length greater than "${this.max_lenght}"`
+      ],
+    ]
+  }
 }
+
